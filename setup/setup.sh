6524d23afd5d8476c4abf919df2b4b26048c61a4
Jason B. Hill <jhill@rallydev.com>
2016-04-26 18:57:48 -0600

    added setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+# Sets up the conda environment
+# Requires anaconda/conda: https://www.continuum.io/downloads
 
+set -x
+
+# Determine if the environment already exists
+ASTROTURF_EXISTS="$(conda env list | grep "^astroturf" | grep -c "astroturf$")"
+echo "ASTROTURF_EXISTS=$ASTROTURF_EXISTS"
+
+# Install it if it doesn't exist
+if [[ "$ASTROTURF_EXISTS" -eq "0" ]]; then
+    echo "creating environment"
+    conda env create -f astroturf.yml
+else
+    echo "environment already exists"
+fi
